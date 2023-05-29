ef5df2366d4d03b47ffff19fb51aec46850bb6d2
Raphael Souza <raphael.pinheiro.souza@gmail.com>
2023-05-29 13:17:47 -0400

    add missing changes

--- follow.py
+++ follow.py
@@ -10,7 +10,7 @@ from viam.services.vision import VisionClient
 
 
 async def connect():
-    #viam1
+    # viam1
     creds = Credentials(
         type="robot-location-secret",
         payload=os.environ["ROBOT_SECRET"],
@@ -21,7 +21,7 @@ async def connect():
     return await RobotClient.at_address(os.environ["ROBOT_LOCATION"], opts)
 
 
-async def is_color_in_front(camera, vis, detector_name):
+async def is_color_in_front(camera, vis):
     """
     Returns whether the appropriate path color is detected in front of the center of the robot.
     """
@@ -31,9 +31,9 @@ async def is_color_in_front(camera, vis, detector_name):
 
     # Crop the image to get only the middle fifth of the top third of the original image
     # cropped_frame = frame.crop((x / 2.5, 0, x / 1.25, y / 3))
-    cropped_frame = frame.crop((x * .4, y*.75, x *.6, y))
+    cropped_frame = frame.crop((x * 0.4, y * 0.75, x * 0.6, y))
 
-    detections = await vis.get_detections(cropped_frame)#, detector_name)
+    detections = await vis.get_detections(cropped_frame)  # , detector_name)
     print(f"front: {detections}")
 
     if detections != []:
@@ -41,7 +41,7 @@ async def is_color_in_front(camera, vis, detector_name):
     return False
 
 
-async def is_color_there(camera, vis, detector_name, location):
+async def is_color_there(camera, vis, location):
     """
     Returns whether the appropriate path color is detected to the left/right of the robot's front.
     """
@@ -50,15 +50,27 @@ async def is_color_there(camera, vis, detector_name, location):
 
     if location == "left":
         # Crop image to get only the left two fifths of the original image
-        cropped_frame = frame.crop((0, y*.5, x *.45, y))
+        cropped_frame = frame.crop((0, y * 0.5, x * 0.45, y))
 
     elif location == "right":
         # Crop image to get only the right two fifths of the original image
-        cropped_frame = frame.crop((x *.55, y*.5, x, y))
+        cropped_frame = frame.crop((x * 0.55, y * 0.5, x, y))
 
-    detections = await vis.get_detections(cropped_frame)#, detector_name)
+    detections = await vis.get_detections(cropped_frame)  # , detector_name)
 
     print(f"{location}: {detections}")
+    if detections != []:
+        return True
+    return False
+
+
+async def is_station(camera, vis):
+    frame = await camera.get_image(mime_type="image/jpeg")
+    x, y = frame.size[0], frame.size[1]
+    cropped_frame = frame.crop((0, y * 0.9, x, y))
+
+    detections = await vis.get_detections(cropped_frame)
+
     if detections != []:
         return True
     return False
@@ -82,7 +94,10 @@ async def main():
         camera = Camera.from_robot(robot, "cam")
         base = Base.from_robot(robot, "viam_base")
 
-        vision = VisionClient.from_robot(robot, "green_detector")
+        green_vision = VisionClient.from_robot(robot, "green_detector")
+        pink_vision = VisionClient.from_robot(robot, "pink_detector")
+
+        # import ipdb; ipdb.set_trace()
 
         # # Put your detector name in place of "green_detector"
         # detections = await vision.get_detections_from_camera("cam")
@@ -98,21 +113,28 @@ async def main():
         angular_power = 0.25
 
         # The main control loop
-        # Put your detector name in place of "green_detector"
         while counter <= 3:
-            while await is_color_in_front(camera, vision, "green_detector"):
+            if await is_station(camera, pink_vision):
+                print("pink detected")
+                await base.stop()
+
+                asyncio.sleep(4)
+                # Play sound
+                break
+
+            while await is_color_in_front(camera, green_vision):
                 print("going straight")
                 # Moves the base slowly forward in a straight line
                 await base.set_power(Vector3(y=linear_power), Vector3())
                 counter == 0
 
             # If there is green to the left, turns the base left at a continuous, slow speed
-            if await is_color_there(camera, vision, "green_detector", "left"):
+            if await is_color_there(camera, green_vision, "left"):
                 print("going left")
                 await base.set_power(Vector3(), Vector3(z=angular_power))
                 counter == 0
             # If there is green to the right, turns the base right at a continuous, slow speed
-            elif await is_color_there(camera, vision, "green_detector", "right"):
+            elif await is_color_there(camera, green_vision, "right"):
                 print("going right")
                 await base.set_power(Vector3(), Vector3(z=-angular_power))
                 counter == 0
