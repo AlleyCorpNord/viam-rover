4704bc1aad9d9e0142dc940faa7b6a554eaf5b69
Rob Linton <rob@resolvemedicalbills.com>
2023-07-05 11:15:21 -0400

    Moved auto-config to roboacn, replaced print with log

--- src/robo/cartorover/__main__.py
+++ src/robo/cartorover/__main__.py
@@ -1,25 +1,22 @@
 import asyncio
-import os
+import logging
 
 from viam.components.camera import Camera
 
 from ..roboacn.utils import connect
 from .pointcloud import PointCloud
 
+log = logging.getLogger("robo.cartorover")
+
 
 async def main():
-    async with connect(
-        os.environ["ROBOT_SECRET"],
-        os.environ["ROBOT_LOCATION"],
-        os.environ.get("ROBOT_AUTH_ENTITY"),
-    ) as robot:
+    async with connect() as robot:
         slam_lidar = Camera.from_robot(robot, "rplidar-component")
 
         while True:
             clouddata, mimetype = await slam_lidar.get_point_cloud()
             dist, point = PointCloud.from_bytes(clouddata).closest_point()
-            print(f"closest point is: {dist} ({point.tolist()})")
+            log.info(f"closest point is: {dist} ({point.tolist()})")
 
-print("Starting up...")
+
 asyncio.run(main())
-print("Done.")
